0dc738ce784b0a493ace93ff5cfe7bb1973b803f
Norwal <jfk1307@g.rit.edu>
2016-10-12 15:38:29 -0400

    Class Layout

---
+++ main/src/Employee.java
@@ -0,0 +1,5 @@
+/**
+ * Created by John King on 12-Oct-16.
+ */
+public class Employee {
+}
---
+++ main/src/Main.java
@@ -0,0 +1,8 @@
+/**
+ * Created by John King on 12-Oct-16.
+ */
+public class Main {
+    public static void main(String args[]){
+
+    }
+}
