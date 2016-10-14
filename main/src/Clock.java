83d9d4814cd36e5ebd168826887027bfa86a8a16
Axiat <nate.ortega95@gmail.com>
2016-10-14 10:54:14 -0400

    - fleshed out Clock class - made the Clock class a singleton

--- main/src/Clock.java
+++ main/src/Clock.java
@@ -2,4 +2,55 @@
  * Created by John King on 12-Oct-16.
  */
 public class Clock {
+    private long startime = 0;
+    private static Clock instance = null;
+
+    // this Enum represents all the states the system can be in
+    public enum States{
+        ARRIVAL_TIME, LUNCHTIME, MANDATORY_MEETING, WORK_TIME
+    }
+
+    /**
+     * This private method exists, just so that a developer cannot
+     * create a clock object the traditional way with "new Clock();"
+     * but instead is forced to use the createClock() method
+     */
+    private Clock(){}
+
+    /**
+     * Ensures that our clock class is a singleton. If there is no clock created
+     * method will return a new clock object, otherwise it will return null
+     * @return
+     */
+    public static Clock createClock(){
+        if(instance == null){
+            instance = new Clock();
+        }
+        else{
+            System.err.println("There can only be one clock instance");
+        }
+        return instance;
+    }
+
+
+    /**
+     * When the Clock object is started, the current time in milliseconds
+     * is logged and used later to calculate the current time in the simulation
+     */
+    public void start(){
+        startime = System.currentTimeMillis();
+    }
+
+
+    /**
+     * is called by an employee and the current time and state is calculated
+     * and the enum representing which block of time it is is returned
+     */
+    public void getTime(){ // TODO: change signature
+
+    }
+
+
+
+
 }
