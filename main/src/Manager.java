57c3376947b0dfa94d13ed2e860b6a9f261fa357
Axiat <nate.ortega95@gmail.com>
2016-10-17 10:26:35 -0400

    - added new states to State enum - created teamlead contstructor

--- main/src/Manager.java
+++ main/src/Manager.java
@@ -2,4 +2,8 @@
  * Created by John King on 12-Oct-16.
  */
 public class Manager {
+    public Manager(){
+
+    }
+
 }
--- main/src/TeamLead.java
+++ main/src/TeamLead.java
@@ -1,5 +1,17 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+
 /**
  * Created by John King on 12-Oct-16.
  */
 public class TeamLead {
+    private ArrayList<Employee> employees;
+    private Manager manager;
+    private LinkedList<Employee> questionQueue = new LinkedList<>();
+
+    public TeamLead(Manager manager, ArrayList<Employee> employees){
+        this.employees = employees;
+        this.manager = manager;
+    }
+
 }
