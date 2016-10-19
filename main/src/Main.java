8af06ae6b875a8ef786024218b2f9eef36d4bcff
Norwal <jfk1307@g.rit.edu>
2016-10-19 10:50:16 -0400

    Started main method

--- main/src/Main.java
+++ main/src/Main.java
@@ -3,6 +3,8 @@
  */
 public class Main {
     public static void main(String args[]){
-
+        final int NUM_EMPLOYEES = 9;
+        final int NUM_TEAM_LEADS = 3;
+        Clock clock = Clock.createClock();
     }
 }
