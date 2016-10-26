6a7f76ccd8de4b7d3c184587048913645bbe541a
Tyler Schoen <tyler@ethode.com>
2016-10-26 16:31:12 -0400

    Fixing stack trace prints for real

--- main/src/ConferenceRoom.java
+++ main/src/ConferenceRoom.java
@@ -28,7 +28,7 @@ public class ConferenceRoom{
             try {
                 wait();
             } catch (InterruptedException e) {
-                System.out.println("Exception: " + e.getStackTrace());
+                e.printStackTrace(System.out);
             }
         }
     }
